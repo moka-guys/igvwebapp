465a2971bfbae4a7f02cee3a928066135fbab0c0
David Brawand <dbrawand@gmail.com>
2022-05-24 15:30:43 +0100

    updated test environment

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Building test version 
-docker buildx build -f Dockerfile.test --platform linux/amd64 -t seglh/igvwebapp:test .
+docker buildx build --no-cache -f Dockerfile.test --platform linux/amd64 -t seglh/igvwebapp:test .
 docker run --rm -it -p 8085:8085 -v `pwd`/test:/igv/resources/dynamic seglh/igvwebapp:test
